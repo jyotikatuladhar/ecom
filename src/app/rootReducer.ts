30dd411f33b6c8323fa3c26b00c75cfcc39ef670
Jyotika <jyotikatuladhar@gmail.com>
2026-01-13 01:31:16 -0600

    add search product and slice

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -2,9 +2,11 @@ import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from "@/features/auth/authSlice";
 import { baseApi } from "@/app/api/baseApi";
 import counterReducer from "@/features/counter/counterSlice";
+import searchReducer from "@/features/catalog/components/SearchBar/SearchInput/searchSlice";
 
 export const rootReducer = combineReducers({
-    auth: authReducer,
     [baseApi.reducerPath]: baseApi.reducer,
-    counter: counterReducer
+    counter: counterReducer,
+    search: searchReducer,
+    auth: authReducer,
 });
--- src/features/catalog/components/SearchBar/SearchInput/SearchInput.tsx
+++ src/features/catalog/components/SearchBar/SearchInput/SearchInput.tsx
@@ -1,8 +1,86 @@
 import { Input } from "antd"
 import styles from "./SearchInput.module.css"
+import { useEffect, useRef, useState, } from "react"
+import type { ChangeEventHandler, ChangeEvent, KeyboardEventHandler, SyntheticEvent } from "react";
+import { useAppDispatch, useAppSelector } from "@/app/hooks";
+import { selectInput, setSearchInput } from "./searchSlice";
+import { useLazySearchProductsQuery } from "@/features/catalog/api/catalogApi";
+import { Loading } from "@/components/Loading";
+import { EmptyState } from "@/components/EmptyState";
+import { useNavigate } from "react-router-dom";
 
 export const SearchInput = () => {
-    return <div >
-        <Input.Search size="large" variant="borderless" placeholder="Search Products" className={styles.search} />
+    const navigate = useNavigate();
+    const [open, setOpen] = useState<boolean>(false)
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    const dispatch = useAppDispatch();
+
+    const [triggerSearchProducts, { data: searchResults, isUninitialized, isLoading, reset }] = useLazySearchProductsQuery();
+
+
+    useEffect(() => {
+        const handler = (e: MouseEvent) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(e.target as Node)) {
+                setOpen(false);
+            }
+        }
+        document.addEventListener('mousedown', handler);
+        return () => document.removeEventListener('mousedown', handler)
+    }, [])
+
+    const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e: ChangeEvent<HTMLInputElement>) => {
+        // implement debounce input
+        const input = e.target.value;
+        if (input.length > 3) {
+            dispatch(setSearchInput(e.target.value))
+        }
+    }
+
+    const handleSearch = (value: string) => {
+        // console.log(value);
+        // dispatch(searchAsync(value))
+        const searchParams = new URLSearchParams();
+        searchParams.set('q', value)
+        searchParams.set('limit', '10')
+        // navigate(`/products/search?${searchParams.toString()}`)
+        triggerSearchProducts({ searchParams })
+    }
+
+    const SearchResults = () => <ul>
+        {
+            searchResults?.items.map(product => {
+                return <li>{product.productName}</li>
+            })
+        }
+    </ul>
+
+    let searchContent;
+    if (isUninitialized) {
+        searchContent = <EmptyState />
+    } else if (isLoading) {
+        searchContent = <Loading />
+    } else if (searchResults) {
+        searchContent = <SearchResults />
+    } else {
+        searchContent = <EmptyState />
+    }
+
+
+    return <div className={`${styles.searchBox} ${open ? styles.active : ""}`}
+        ref={dropdownRef}>
+        <Input.Search onClick={() => setOpen(true)}
+            // onBlur={() => setOpen(false)}
+            size="large"
+            // variant="borderless"
+            placeholder="Search Products" className={styles.searchInput}
+            onChange={handleInputChange}
+            onSearch={handleSearch}
+            onClear={reset}
+        />
+        {/* Dropdown */}
+        <div className={`${styles.searchOptions} `}>
+            {searchContent}
+        </div>
     </div>
 }
